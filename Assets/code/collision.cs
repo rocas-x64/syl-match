66ff6e97439ad8f9f11f0d4ce5dd42d709dcbe99
RocasWOW <260527491+rocas-x64@users.noreply.github.com>
2026-02-28 15:44:25 -0500

    Added detection collision individually for each player

--- Assets/code/collision.cs
+++ Assets/code/collision.cs
@@ -3,24 +3,41 @@ using UnityEngine;
 public class collision : MonoBehaviour
 {
 private bool playerInside = false;
+private bool player2Inside = false;
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player")){
             playerInside = true;
-            Debug.Log("player is inside\n");
+            Debug.Log("Player 1 is inside\n");
+        }
+        else if (other.CompareTag("Player2")){
+            player2Inside = true;
+            Debug.Log("Player 2 is inside\n");
+            }
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player")){
             playerInside = false;
-            Debug.Log("player is outside\n");
+            Debug.Log("Player 1 is outside\n");
+        }
+        else if (other.CompareTag("Player2"))
+        {
+            player2Inside = false;
+            Debug.Log("Player 2 is outside\n");
+        }
     }
 
     void Update()
     {
-        if (playerInside && Input.GetKeyDown(KeyCode.E))
+        if (playerInside && Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            TriggerInteraction();
+        }
+
+        if (player2Inside && Input.GetKeyDown(KeyCode.RightShift))
         {
             TriggerInteraction();
         }
