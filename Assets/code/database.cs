d600afae083f1e57951886a8c164e4317006af35
RocasWOW <260527491+rocas-x64@users.noreply.github.com>
2026-02-28 16:33:07 -0500

    database init

---
+++ Assets/code/database.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class database : MonoBehaviour
+{
+    
+    public List<string> syllables = new List<string>{
+        "a",
+    "gneau",
+    };
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
