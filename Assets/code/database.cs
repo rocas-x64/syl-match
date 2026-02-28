1d8a7c07a72937c4d89ebcd357916bb29ab10cf3
RocasWOW <260527491+rocas-x64@users.noreply.github.com>
2026-02-28 17:50:43 -0500

    probably made Word class

--- Assets/code/database.cs
+++ Assets/code/database.cs
@@ -4,11 +4,74 @@ using UnityEngine;
 public class database : MonoBehaviour
 {
     
-    public List<string> syllables = new List<string>{
-        "a",
+    /*public List<string> syllables = new List<string>
+    {
+    "a",
     "gneau",
+    "bi",
+    "by",
+    "bie",
+    "jou",
+    "hi",
+    "bou",
+    "ai",
+    "der",
+    "as",
+    "soir",
+    "poi",
+    "sson",
+    "son",
+    "boi",
+    "bou",
+    "chon"
+    }; */
+
+    public List<string> phonemes = new List<string>
+    {
+        "a",
+        "gn",
+        "ni",
+        "o",
+        "eau",
+        "au",
+        "bi",
+        "by",
+        "bie",
+        "g",
+        "j",
+        "ju",
+        "ou",
+        "u",
+        "hou",
+        "hi",
+        "hie",
+        "hy",
+        "b",
+        "ai",
+        "é",
+        "et",
+        "d",
+        "er",
+        "ez",
+        "s",
+        "ss",
+        "sw",
+        "oi",
+        "oua",
+        "r"
     };
     
+public class Word
+{
+    public string name;
+    public List<string> phonemes;
+
+    public Word(string name, List<string> phonemes)
+    {
+        this.name = name;
+        this.phonemes = phonemes;
+    }
+}
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
